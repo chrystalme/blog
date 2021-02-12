5b1aeefded93c7b2128a8a6ac5b555789526b5a7
chrystalme <afam.ifeanyi@gmail.com>
2021-02-12 11:39:36 +0200

    Add delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,17 +31,19 @@ class ArticlesController < ApplicationController
     if @article.update(article_params)
       redirect_to @article
     else
-      render :edit, ststus: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @article = Article.find(params[:id])
-    
+    @article.destroy
+
+    redirect_to root_path
   end
+
   private
-  
-  def article_params
-    params.require(:article).permit(:title, :body)
-  end
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
